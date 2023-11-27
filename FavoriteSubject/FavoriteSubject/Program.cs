589f2c6eae3d82532939ba31b4ca3686dfd5da3d
Jon Frederick <jonFred151@gmail.com>
2023-11-26 21:30:37 -0600

    Added a switch cause program to the file.

---
+++ FavoriteSubject/FavoriteSubject/Program.cs
@@ -0,0 +1,60 @@
+﻿
+
+
+namespace favoriteSubject
+{
+
+
+    internal class program
+    {
+
+        public static void Main(string[] args)
+        {
+            string subject;
+
+            Console.WriteLine("What is your favorite subject of these option. \nMath \nEnglish \nScience \nHistory \nLunch");
+            subject = Console.ReadLine();
+            switch (subject)
+            {
+                case "Math" or "math":
+                    Console.WriteLine("Favorite Subject is Math");
+                    break;
+                case "English" or "english" :
+                    Console.WriteLine("Favorite Subject is English");
+                    break;
+                case "Science" or "science":
+                    Console.WriteLine("Favorite Subject is Science");
+                    break;
+                case "History" or "history":
+                    Console.WriteLine("Favorite Subject is History");
+                    break;
+                case "Lunch" or "lunch":
+                    Console.WriteLine("Favorite Subject is Lunch");
+                    break;
+
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
